799bdcdc07a19abfee335bb2aea42777d7df785b
payal1206 <68314999+payal1206@users.noreply.github.com>
2022-10-08 21:24:40 +0530

    Create 37_Maximum Product Subarray.cpp

---
+++ Arrays/37_Maximum Product Subarray.cpp
@@ -0,0 +1,38 @@
+// C++ program to find Maximum Product Subarray
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Returns the product of max product subarray.*/
+int maxSubarrayProduct(int arr[], int n)
+{
+	// Initializing result
+	int result = arr[0];
+
+	for (int i = 0; i < n; i++)
+	{
+		int mul = arr[i];
+		// traversing in current subarray
+		for (int j = i + 1; j < n; j++)
+		{
+			// updating result every time
+			// to keep an eye over the maximum product
+			result = max(result, mul);
+			mul *= arr[j];
+		}
+		// updating the result for (n-1)th index.
+		result = max(result, mul);
+	}
+	return result;
+}
+
+// Driver code
+int main()
+{
+	int arr[] = { 1, -2, -3, 0, 7, -8, -2 };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << "Maximum Sub array product is "
+		<< maxSubarrayProduct(arr, n);
+	return 0;
+}
+
+// This code is contributed by Aditya Kumar (adityakumar129)
